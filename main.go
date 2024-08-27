9310e26cfec9fdbad9791004948966ca7f227db1
wrqqqr <wrqqqrrr@rambler.ru>
2024-08-27 06:56:29 +0300

    progress

--- main.go
+++ main.go
@@ -5,18 +5,34 @@ import (
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	const headersContent = "text/plain; charset=utf-8"
+	const headersKey = "Content-Type"
+	const stsCode = 200
+	bdyText := []byte("OK")
+
+	w.Header().Add(headersKey, headersContent)
+	w.WriteHeader(stsCode)
+	w.Write(bdyText)
+}
+
 func main() {
 	const port = "8080"
+	const fileServerPath = "/app/*"
 	const filepathRoot = "."
+	const trafficPath = "/healthz"
 
 	mux := http.NewServeMux()
+
+	mux.Handle(fileServerPath, http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc(trafficPath, handler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
