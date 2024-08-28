ab8b52f9ffe08964d44300bdcd803effe45d28d7
wrqqqr <wrqqqrrr@rambler.ru>
2024-08-29 02:49:04 +0300

    Added only allowed http requests

--- main.go
+++ main.go
@@ -3,15 +3,46 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) countRequestsHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	conc := "Hits: " + strconv.Itoa(cfg.fileserverHits)
+	w.Write([]byte(conc))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	cfg := apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mdwHandler := cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", mdwHandler))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", cfg.countRequestsHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
